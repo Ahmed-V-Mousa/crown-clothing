b24e14c18b966fe9ea6c614fde4b5a5918d12ae5
ahmed.v.mousa <ahmed.v.mousa@gmail.com>
2022-02-03 18:54:18 +0200

    updating the app with a shop page, collection preview component and collection item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -11,7 +11,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
       className='background-image'
       style={{
         backgroundImage: `url(${imageUrl})`
-      }}
+      }}wd
     />
     <div className='content'>
       <h1 className='title'>{title.toUpperCase()}</h1>
