303f5e93e862145ad5c4cbb26dae757e6dfb4d4c
ahmed.v.mousa <ahmed.v.mousa@gmail.com>
2022-02-12 15:31:12 +0200

    updating stripe integration and updating favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51KSJsMK3qXMfFBD0WnjoyOPYGCrbr5v4MBeJuoDoLzMQXj6RX49BHAFkNURZiJjmwPK9bGJXAi0VPdz7OygWCJxO00FHC5uGKU';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Crown Clothing'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
